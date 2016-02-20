0414b5fbf458330826c96fdc12bdc8314f98b2ce
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-02-19 22:20:06 -0700

    Build components to render skills

---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,7 @@
+var Header = React.createClass({
+  render() {
+    return (
+      <h1>Creact Skills Database</h1>
+    )
+  }
+})
---
+++ app/assets/javascripts/components/_new_skill.js.jsx
@@ -0,0 +1,9 @@
+var NewSkill = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>New Skill</h1>
+      </div>
+    )
+  }
+})
